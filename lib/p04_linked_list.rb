9a9bb9b5644a756b61c40448760276ac00037c11
App Academy Student <student@appacademy.io>
2016-03-18 15:09:01 -0700

    made linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -14,7 +15,12 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +29,63 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    current_node = find(key)
+    current_node.val
   end
 
   def include?(key)
+    return false if find(key) == @tail
+    true
   end
 
   def insert(key, val)
+    new_link = Link.new(key, val)
+    last.next = new_link
+    new_link.prev = last
+    @tail.prev = new_link
+    new_link.next = @tail
   end
 
   def remove(key)
+    current_node = find(key)
+    previous_node = current_node.prev
+    next_node = current_node.next
+    previous_node.next = next_node
+    next_node.prev = previous_node
   end
 
-  def each
+  def find(key)
+    current_node = first
+    until current_node.key == key
+      break if current_node == @tail
+      current_node = current_node.next
+    end
+    current_node
+  end
+
+  def each(&prc)
+    prc ||= Proc.new { |el| return el }
+    current_node = first
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
